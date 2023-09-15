cb8426a84b15b39262168c0f805abb98f5f0e105
Fabian <fabianperez@Fabians-iMac.local>
2023-09-14 23:20:16 -0500

    init

---
+++ test1/test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Fabian Perez on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
