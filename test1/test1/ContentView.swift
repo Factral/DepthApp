50216998037683b1d2737c5dd6fafa1c975cfd9d
Factral <nelsonfabiancs8@gmail.com>
2023-09-15 08:23:17 -0500

    added poker ui

--- test1/test1/ContentView.swift
+++ test1/test1/ContentView.swift
@@ -1,45 +1,68 @@
-	
-    //
-    //  ContentView.swift
-    //  test1
-    //
-    //  Created by Fabian Perez on 14/09/23.
-    //
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Chris Ching on 2022-12-02.
+//
 
-    import SwiftUI
+import SwiftUI
 
-    struct ContentView: View {
+struct ContentView: View {
+    var body: some View {
         
-        var body: some View {
+        ZStack {
             
-            ZStack {
-                Color(.black)
-                    .ignoresSafeArea()
-                
-                VStack {
-                    Image("example")
-                        .resizable()
-                        .cornerRadius(15)
-                        .aspectRatio(contentMode: .fit)
-                        .padding(.all)
-                    Text("Fig 1.")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.white)
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
                 }
+                Spacer()
+                Image("button")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                            
+                        Text("0")
+                            .font(.largeTitle)
+                            
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+                
             }
-            
         }
+        
     }
+}
 
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            Group {
-                ContentView()
-                ContentView()
-                ContentView()
-            }
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
 
-            
