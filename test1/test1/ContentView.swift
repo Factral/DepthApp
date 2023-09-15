95b3dff2af0ca69e839b6d4a3da8c526fc677192
Fabian *-* <nelsonfabiancs8@gmail.com>
2023-09-15 03:13:37 -0500

    basic card

--- test1/test1/ContentView.swift
+++ test1/test1/ContentView.swift
@@ -11,23 +11,62 @@
     struct ContentView: View {
         
         var body: some View {
+
+            Zstack {
+                Color(.systemMINT)
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack(alignment: .leading, spacing:20){
             
-            ZStack {
-                Color(.black)
-                    .ignoresSafeArea()
-                
-                VStack {
                     Image("example")
                         .resizable()
-                        .cornerRadius(15)
                         .aspectRatio(contentMode: .fit)
-                        .padding(.all)
-                    Text("Fig 1.")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.white)
+                        .cornerRadius(15)
+                
+                    HStack {
+                        Text("Niagara falls")
+                            .font(.title)
+                            .fontWeight(.bold)
+
+                        Spacer()
+
+                        VStack {
+
+                            HStack{
+                                Image(systemName: "star.fill").
+                                Image(systemName: "star.fill")
+                                Image(systemName: "star.fill")
+                                Image(systemName: "star.fill")
+                                Image(systemName: "star.leadinghalf.fill")
+                            }
+
+                            Text("(Reviews 361)")
+                        }
+                        .foregroundColor(.yellow)
+                        .font(.caption)
+
+                    }
+
+                    Text("come visit the falls for an experience of a lifetime")
+
+                    HStack{
+                        Spacer()
+                        Image(systemName: "fork.knife")
+                        Image(systemName: "binoculars.fill")
+                    }
+                    .foregroundColor(.gray)
+                    .font(.caption)
                 }
+                .padding()
+                .background(Rectangle()
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 15))
+                .padding()
+
             }
+
+
             
         }
     }
