bca53f3d70f57f94bfee68a4963cf36221454438
Factral <nelsonfabiancs8@gmail.com>
2023-09-15 02:13:42 -0500

    first view test: img,caption,background

--- test1/test1/ContentView.swift
+++ test1/test1/ContentView.swift
@@ -1,21 +1,45 @@
-//
-//  ContentView.swift
-//  test1
-//
-//  Created by Fabian Perez on 14/09/23.
-//
+	
+    //
+    //  ContentView.swift
+    //  test1
+    //
+    //  Created by Fabian Perez on 14/09/23.
+    //
 
-import SwiftUI
+    import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    struct ContentView: View {
+        
+        var body: some View {
+            
+            ZStack {
+                Color(.black)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Image("example")
+                        .resizable()
+                        .cornerRadius(15)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.all)
+                    Text("Fig 1.")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                }
+            }
+            
+        }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            Group {
+                ContentView()
+                ContentView()
+                ContentView()
+            }
+        }
     }
-}
+
+            
