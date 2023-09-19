8c3233569832ae36fe5e1730b9f408edb66f0ab1
Fabian *-* <nelsonfabiancs8@gmail.com>
2023-09-18 22:43:57 -0500

    depth maps data

--- AppDepth/AppDepth/ContentView.swift
+++ AppDepth/AppDepth/ContentView.swift
@@ -1,43 +1,31 @@
-//
-//  ContentView.swift
-//  AppDepth
-//
-//  Created by Fabian Perez on 15/09/23.
-//
-
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
+    @StateObject var viewModel = ARViewModel()
+    
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        ARViewContainer(viewModel: viewModel).edgesIgnoringSafeArea(.all)
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    @ObservedObject var viewModel: ARViewModel
     
     func makeUIView(context: Context) -> ARView {
-        
         let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+        arView.session.delegate = viewModel
         return arView
-        
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
-    
 }
 
-#if DEBUG
-struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class ARViewModel: NSObject, ARSessionDelegate, ObservableObject {
+    @Published var depthMap: CVPixelBuffer?
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        depthMap = frame.sceneDepth?.depthMap
     }
 }
-#endif
